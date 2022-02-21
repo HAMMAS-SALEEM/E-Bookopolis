0aeea23707c4d341aeaaf2f03c6b8c48d1af725f
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-02-21 19:26:49 +0500

    Added components

---
+++ src/components/Books.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Books = () => {
+  const booksArray = [{
+    id: 1, title: 'Dune', author: 'Frank Herbert', genre: 'Science Fiction',
+  }];
+  return (
+    <>
+      <h1 className="books-page-title">E-Bookopolis</h1>
+      <form id="form">
+        <input type="text" id="book-title" name="book-title" className="book" placeholder="Book Name..." />
+        <input type="text" id="book-author" name="book-author" className="book" placeholder="Book Author..." />
+        <input type="text" id="book-genre" name="book-genre" className="book" placeholder="Genre..." />
+        <input type="button" value="Submit" />
+      </form>
+      <ul className="books-container">
+        {
+              booksArray.map((book) => (
+                <li key={book.id}>
+                  <ul>
+                    <li>{book.genre}</li>
+                    <li>{book.title}</li>
+                    <li>{book.author}</li>
+                  </ul>
+                  <input type="button" className="remove-btn" id={book.id} value="Remove" />
+                </li>
+              ))
+          }
+      </ul>
+    </>
+  );
+};
+
+export default Books;
---
+++ src/components/Categories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Categories = () => (
+  <>
+    <h1 className="categories-page-title">Under Construction</h1>
+  </>
+);
+
+export default Categories;
