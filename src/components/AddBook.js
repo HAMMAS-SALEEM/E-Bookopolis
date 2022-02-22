98cd03a52ee07fcb2161f8586b42534771407407
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-02-22 17:06:04 +0500

    addition function completed

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <>
-    <form id="form">
-      <input type="text" id="book-title" name="book-title" className="book" placeholder="Book Name..." />
-      <select name="categories" id="categories-dropdown">
-        <option defaultValue="Categories">Categories</option>
-        <option value="horror">Horror</option>
-        <option value="sci-fic">Sci-Fic</option>
-        <option value="comedy">Comedy</option>
-      </select>
-      <input type="submit" value="Submit" />
-    </form>
-  </>
-);
+const AddBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+  const submitBook = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    if (title && author) {
+      const book = {
+        id: new Date().getTime().toString(),
+        title,
+        author,
+      };
+      dispatch(addBook(book));
+      setTitle('');
+      setAuthor('');
+      form.reset();
+    }
+  };
+  return (
+    <>
+      <form id="form" onSubmit={submitBook}>
+        <input type="text" id="book-title" name="title" className="book" placeholder="Book Name..." onKeyUp={(e) => setTitle(e.target.value)} required />
+        <input type="text" id="book-author" name="author" className="book" placeholder="Book Author..." onKeyUp={(e) => setAuthor(e.target.value)} required />
+        <input type="submit" />
+      </form>
+    </>
+  );
+};
 
 export default AddBook;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,23 +3,25 @@ const REMOVE_BOOKS = 'bookstore/books/REMOVE_BOOKS';
 
 const initialState = [];
 
-export const addBook = playload => ({
-    type: ADD_BOOKS,
-    playload,
-})
+export const addBook = (playload) => ({
+  type: ADD_BOOKS,
+  playload,
+});
 
-export const removeBook = playload => ({
-    type: REMOVE_BOOKS,
-    playload
-})
+export const removeBook = (playload) => ({
+  type: REMOVE_BOOKS,
+  playload,
+});
 
-const reducer = (state=initialState, action) => {
-    switch(action.type) {
-        case ADD_BOOKS:
-            return [...state, action.playload];
-        case REMOVE_BOOKS:
-            return state.filter(book=>book.id===id);
-        default:
-            return state;
-    }
-}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOKS:
+      return [...state, action.playload];
+    // case REMOVE_BOOKS:
+    //   return state.filter((book) => book.id === id);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
