70417292a6d8bef1bb42af3c7a9bc609a3e6ab3f
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-02-22 20:38:54 +0500

    Successfully added redux to application

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,33 +1,19 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/books';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const AddBook = () => {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const dispatch = useDispatch();
-  const submitBook = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const book = {
-      id: new Date().getTime().toString(),
-      title,
-      author,
-    };
-    dispatch(addBook(book));
-    setTitle('');
-    setAuthor('');
-    form.reset();
-  };
-  return (
-    <>
-      <form id="form" onSubmit={submitBook}>
-        <input type="text" id="book-title" name="title" className="book" placeholder="Book Name..." onChange={(e) => setTitle(e.target.value)} required />
-        <input type="text" id="book-author" name="author" className="book" placeholder="Book Author..." onChange={(e) => setAuthor(e.target.value)} required />
-        <input type="submit" />
-      </form>
-    </>
-  );
+const AddBook = ({ submitBook, bookDetail }) => (
+  <>
+    <form id="form" onSubmit={submitBook}>
+      <input type="text" id="book-title" name="title" className="book" placeholder="Book Name..." onChange={bookDetail} required />
+      <input type="text" id="book-author" name="author" className="book" placeholder="Book Author..." onChange={bookDetail} required />
+      <input type="submit" />
+    </form>
+  </>
+);
+
+AddBook.propTypes = {
+  submitBook: PropTypes.func.isRequired,
+  bookDetail: PropTypes.func.isRequired,
 };
 
 export default AddBook;
--- src/components/SingleBook.js
+++ src/components/SingleBook.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/books';
 
-const SingleBook = ({ book }) => {
-  const dispatch = useDispatch();
-  const removeSingleBook = (e) => {
-    const { id } = e.target;
-    dispatch(removeBook(id));
-  };
-  return (
-    <>
-      <ul>
-        <li>{book.title}</li>
-        <li>{book.author}</li>
-      </ul>
-      <input type="button" className="remove-btn" onClick={removeSingleBook} id={book.id} value="Remove" />
-    </>
-  );
-};
+const SingleBook = ({ book, removeSingleBook }) => (
+  <>
+    <ul>
+      <li>{book.title}</li>
+      <li>{book.author}</li>
+    </ul>
+    <input type="button" className="remove-btn" onClick={removeSingleBook} id={book.id} value="Remove" />
+  </>
+);
 
 export default SingleBook;
 
@@ -28,4 +19,5 @@ SingleBook.propTypes = {
     author: PropTypes.string,
     id: PropTypes.string,
   }).isRequired,
+  removeSingleBook: PropTypes.func.isRequired,
 };
