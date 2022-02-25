4499df741e9dfcf79ad79c8c3a058cc647739391
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-02-25 19:30:41 +0500

    Updated styling of books section

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import AddBook from './AddBook';
 import SingleBook from './SingleBook';
+import './Books.css';
 
 const Books = () => (
-  <>
+  <section className="books-section">
     <ul className="books-container">
       <SingleBook />
     </ul>
     <AddBook />
-  </>
+  </section>
 );
 
 export default Books;
--- src/components/SingleBook.js
+++ src/components/SingleBook.js
@@ -29,28 +29,38 @@ const SingleBook = () => {
       books.length === 0
         ? 'No Books Available'
         : books.map((book) => (
-          <li key={book.item_id}>
+          <li className="single-book" key={book.item_id}>
             <ul>
               <li>
-                <ul className="book-details">
-                  <li>
-                    <ul>
-                      <li>{book.title}</li>
-                      <li>{book.category}</li>
+                <ul>
+                  <li className="book-details">
+                    <ul className="book-api-details-container">
+                      <li className="book-category">{book.category}</li>
+                      <li className="book-title">{book.title}</li>
                     </ul>
-                  </li>
-                  <li><CircularProgressbar className="circular-progress" value={percentage} text={`${percentage}%`} /></li>
-                  <li><div className="horizontal-line"> </div></li>
-                  <li>
-                    <ul>
-                      <li>Current Chapter</li>
-                      <li>Chapter 17</li>
-                      <li><button type="button">Update Progress</button></li>
+                    <ul className="progress-details-container">
+                      <li><CircularProgressbar className="circular-progress" value={percentage} text={`${percentage}%`} /></li>
+                      <li><div className="horizontal-line"> </div></li>
+                      <li>
+                        <ul>
+                          <li>Current Chapter</li>
+                          <li>Chapter 01</li>
+                          <li><button type="button">Update Progress</button></li>
+                        </ul>
+                      </li>
                     </ul>
                   </li>
                 </ul>
               </li>
-              <li><input type="button" className="remove-btn" id={book.item_id} onClick={sendToAPI} value="Remove" /></li>
+              <li>
+                <ul className="book-btn-container">
+                  <li><input type="button" className="comments-btn btn" value="Comments" /></li>
+                  <li><div className="small-horizontal-line"> </div></li>
+                  <li><input type="button" className="remove-btn btn" id={book.item_id} onClick={sendToAPI} value="Remove" /></li>
+                  <li><div className="small-horizontal-line"> </div></li>
+                  <li><input type="button" className="edit-btn btn" value="Edit" /></li>
+                </ul>
+              </li>
             </ul>
           </li>
         ))
